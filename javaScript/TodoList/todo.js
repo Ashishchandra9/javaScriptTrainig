3084d5741d923b518910fe4a158b193799694c34
ashish.chandra1 <ashish.chandra@tibilsolutions.com>
2022-12-21 05:41:32 +0530

    added script file and todo

--- javaScript/TodoList/todo.js
+++ javaScript/TodoList/todo.js
@@ -4,14 +4,13 @@ const display = document.getElementById('display');
 
 addbutton.addEventListener('click',function(){
     var para = document.createElement('p');
-    para.classList.add('paragraph-styling');
     para.innerText = input.value;
     display.appendChild(para);
     input.value = "";
     para.addEventListener('click',function(){
         para.style.textDecoration = "line-through";
     })
-    para.addEventListener('dbclick',function(){
+    para.addEventListener('dblclick',function(){
         display.removeChild(para);
     })
 })
